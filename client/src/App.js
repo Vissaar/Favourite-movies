2f548dde4c7723cd865517b5fc5b05687d96ebff
Vissaar <vissarshabani@gmail.com>
2024-01-15 10:19:18 +0000

    Text change

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cool Movie Recommendation App</h1>
+        <h1>Favourite movies App</h1>
         <div className="movie-search">
           <input
             type="text"
